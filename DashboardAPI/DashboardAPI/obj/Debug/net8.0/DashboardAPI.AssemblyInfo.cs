725dde18a66af267d6d8be63a2393225b18b26a5
Bruno0M <brunomedeirosr09@gmail.com>
2024-03-01 16:20:52 -0300

    configurando docker

--- DashboardAPI/DashboardAPI/obj/Debug/net8.0/DashboardAPI.AssemblyInfo.cs
+++ DashboardAPI/DashboardAPI/obj/Debug/net8.0/DashboardAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DashboardAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b521392767cb82d765929cdb3b20b5e4958867")]
 [assembly: System.Reflection.AssemblyProductAttribute("DashboardAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DashboardAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
