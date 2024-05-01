92eb4a840add494d6b61296bd8b6550b539fb54d
KaykySiq <josekayky@hotmail.com>
2024-05-01 16:17:02 -0300

    feat: adiciona o nome e sobrenome do usuário à tela do Dashboard

---
+++ DashboardUi/src/services/user-services.js
@@ -0,0 +1,31 @@
+import { environment } from "../environments/environments.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+    const apiUrl = environment.ApiUrl
+
+
+    var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIyMDAyIiwiZW1haWwiOiJzdHJpbmcyQGdtYWlsLmNvbSIsIm5iZiI6MTcxNDU4ODc3MywiZXhwIjoxNzE0NTg4ODkzLCJpYXQiOjE3MTQ1ODg3NzN9.inrvNkH4HKeEvy1_z3uU8KROp85txdZUmn02eKK0hjA"
+
+    var options = {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+            Authorization: 'Bearer ' + token
+        },
+
+    }
+
+    fetch(`${apiUrl}/User`, options)
+        .then(response => {
+            return response.json()
+
+        })
+        .then(data => {
+                const h3 = document.createElement("h3");
+                h3.textContent = data.data[0].firstName + ' ' + data.data[0].lastName;
+
+                var name = document.getElementById("user");
+                name.appendChild(h3);
+            });
+
+});
