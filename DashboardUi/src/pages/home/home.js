44101bd83aba4a33ba904044bd16f6c71d84838a
KaykySiq <josekayky@hotmail.com>
2024-05-09 22:45:30 -0300

    refactor: Altera rotas e Ajusta o tamanho do modal.

--- DashboardUi/src/pages/home/home.js
+++ DashboardUi/src/pages/home/home.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  event.preventDefault();
 
-  const btnRegister = document.getElementById('btn-register');
-
-  btnRegister.addEventListener('click', () => {
-    window.location.href = '../auth/auth.html?active=true';
+  event.preventDefault()
+  
+    const btnRegister = document.getElementById('btn-register');
+    btnRegister.addEventListener('click', () => {
+      window.location.href = '/pages/auth/index.html?active=true';
+    });
   });
-
-});
+  
