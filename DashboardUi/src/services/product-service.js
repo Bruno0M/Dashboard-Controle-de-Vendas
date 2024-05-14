71c005002f15b5af13ff780342ab3b6fe58bd478
Bruno0M <brunomedeirosr09@gmail.com>
2024-05-13 22:33:18 -0300

    feat: Adiciona botão de excluir produto

--- DashboardUi/src/services/product-service.js
+++ DashboardUi/src/services/product-service.js
@@ -1,18 +1,17 @@
 import { devEnvironment, environment } from "../environments/environments.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";
 
+var token = localStorage.getItem('token');
+const apiUrl = `${environment.ApiUrl}/Product`
 
 document.addEventListener("DOMContentLoaded", function () {
   if (!isAuthenticated()) window.location.href = "/pages/auth/";
 
-  const apiUrl = `${environment.ApiUrl}/Product`
 
-  var token = localStorage.getItem('token');
 
   document.getElementById("btnSalvar").addEventListener("click", function (event) {
     event.preventDefault();
 
-    
     var categoria = document.getElementById("m-categoria").value;
     var produto = document.getElementById("m-produto").value;
     var valor = document.getElementById("m-valor").value;
@@ -37,13 +36,16 @@ document.addEventListener("DOMContentLoaded", function () {
     fetch(`${apiUrl}`, options)
       .then(response => {
         if (!response.ok) throw new Error(response.statusText());
-        return response;
+        return response.json();
       })
-      
+      .then(newProduct => {
+        const newRow = insertItem(newProduct);
+        document.getElementById('product-rows').insertAdjacentHTML('beforeend', newRow);
+      })
       .then(json => console.log(json))
       .catch(err => console.log(err));
-
   });
+
 
   var options = {
     method: "GET",
@@ -63,16 +65,56 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function insertItem(product) {
-  return `
+  const row = `
     <tr>
     <td>${product.categoria}</td>
     <td>${product.name}</td>
     <td>R$ ${product.price}</td>
     <td>${product.quantidade}</td>
+    <td class="acao">
+      <button onclick="editItem()"><i class='bx bx-edit'></i></button>
+    </td>
+    <td>
+      <button id="${product.id}"><i class='bx bx-trash'></i></button>
+    </td>
     </tr>`;
+
+  setTimeout(() => {
+    const deleteButton = document.getElementById(product.id);
+    deleteButton.addEventListener('click', () => {
+      console.log(product.id)
+      deleteItem(product.id);
+    });
+  }, 0);
+
+  return row;
 }
 
 function displayProducts(data) {
   const productRows = data.data.map(product => insertItem(product));
   document.getElementById('product-rows').innerHTML = productRows.join('');
+}
+
+
+function deleteItem(productId) {
+  var options = {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+      Authorization: 'Bearer ' + token
+    },
+  }
+
+  const row = document.getElementById(productId).parentNode.parentNode;
+
+  fetch(`${apiUrl}?id=${productId}`, options)
+    .then(response => {
+      if (response.ok) {
+        row.remove();
+        console.log("ok")
+      } else {
+        console.error('Erro ao excluir produto:', response.status);
+      }
+    })
+    .catch(error => console.error('Erro na requisição:', error));
 }
