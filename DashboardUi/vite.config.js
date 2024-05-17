b999493554a7070df2367cf1308d19cef3f75586
Tiago Santos <tiagosantos.eli@gmail.com>
2024-05-17 11:54:14 -0300

    feat: adiciona a pagina de configuração

--- DashboardUi/vite.config.js
+++ DashboardUi/vite.config.js
@@ -1,4 +1,4 @@
-import { resolve } from 'path'
+import { dirname, resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig ({
@@ -12,7 +12,8 @@ export default defineConfig ({
         auth: resolve(__dirname, '/pages/auth/index.html'),
         dashboard: resolve(__dirname, '/pages/dashboard-home/index.html'),
         produtos: resolve(__dirname, '/pages/dashboard-produtos/index.html'),
-        perfil: resolve(__dirname, '/pages/dashboard-perfil/index.html')
+        perfil: resolve(__dirname, '/pages/dashboard-perfil/index.html'),
+        config: resolve(__dirname, '/pages/dashboard-config/index.html'),
       }
     }
   },
